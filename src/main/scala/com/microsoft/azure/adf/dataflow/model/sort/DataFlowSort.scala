ea66a99449a55a37d77ae661dab9c1ee51447472
arunsethia <arunsethia@microsoft.com>
2023-08-16 13:20:44 -0500

    added code generation for Scala Spark and PySpark

--- src/main/scala/com/microsoft/azure/adf/dataflow/model/sort/DataFlowSort.scala
+++ src/main/scala/com/microsoft/azure/adf/dataflow/model/sort/DataFlowSort.scala
@@ -1,7 +1,7 @@
 package com.microsoft.azure.adf.dataflow.model.sort
 
-import com.microsoft.azure.adf.dataflow.model.{Expr, SparkCodeGenerator}
 import com.microsoft.azure.adf.dataflow.model.util.ListKeyValueProperties
+import com.microsoft.azure.adf.dataflow.model.{Expr, SparkCodeGenerator}
 
 /**
  * Data flow Sort transformation
@@ -13,8 +13,26 @@ import com.microsoft.azure.adf.dataflow.model.util.ListKeyValueProperties
  */
 case class DataFlowSort(inputVarName: String, lstSortField: List[SortField],
                         sortProperties: ListKeyValueProperties, output_assigned_variable: String) extends Expr
-                        with SparkCodeGenerator {
-  override def scalaSparkCode(): String = ???
+  with SparkCodeGenerator {
+  override def scalaSparkCode(): String = {
+    s"""val ${output_assigned_variable}=${inputVarName}.select("*").orderBy(${getSortColumnOperation()})"""
+  }
 
-  override def pySparkCode(): String = ???
+  override def pySparkCode(): String = {
+    s"""${output_assigned_variable}=${inputVarName}.select("*").orderBy(${getSortColumnOperation()})"""
+  }
+
+
+  private def getSortColumnOperation(): String = {
+    lstSortField.map(sf => {
+      val sortDirection: String = sf.nullFirst match {
+        case true =>
+          // at the end open and closing brackets are require for PySpark
+          // for scala spark it is optional
+          s"""${sf.sortDirection}_nulls_first()"""
+        case _ => s"""${sf.sortDirection}()"""
+      }
+      s"""col("${sf.fieldName}").${sortDirection}"""
+    }).mkString(",")
+  }
 }
