563fc2f7969e04746f303e80ff63d53f686fb8b9
arunsethia <arunsethia@microsoft.com>
2023-08-16 10:59:51 -0500

    fixed issues related to PySpark and Scala Spark - different way of handling field with the source and logical operators

--- src/main/scala/com/microsoft/azure/adf/dataflow/model/filter/DataFlowFilter.scala
+++ src/main/scala/com/microsoft/azure/adf/dataflow/model/filter/DataFlowFilter.scala
@@ -1,5 +1,8 @@
 package com.microsoft.azure.adf.dataflow.model.filter
 
+import com.microsoft.azure.adf.dataflow.`type`.LanguageType.{Language, PYTHON, SCALA}
+import com.microsoft.azure.adf.dataflow.constant.LogicalConditionMapping.{DATAFLOW_LOGICAL_COND_MAP_PY, DATAFLOW_LOGICAL_COND_MAP_SCALA}
+import com.microsoft.azure.adf.dataflow.constant.LogicalOperatorMapping.{DATAFLOW_LOGICAL_OPERATOR_MAP_PY, DATAFLOW_LOGICAL_OPERATOR_MAP_SCALA}
 import com.microsoft.azure.adf.dataflow.model.{Expr, SparkCodeGenerator}
 import com.microsoft.azure.adf.util.StringUtil
 
@@ -7,13 +10,11 @@ import com.microsoft.azure.adf.util.StringUtil
  * This model class provides filter operation detail for the spark code generation
  *
  * @param inputVarName             input variable name
- * @param lstExpression          list of filter expressions
+ * @param lstExpression            list of filter expressions
  * @param output_assigned_variable output variable name
  */
 case class DataFlowFilter(inputVarName: String, lstExpression: ListExpressionCondition, output_assigned_variable: String)
   extends Expr with SparkCodeGenerator {
-
-  private val DATAFLOW_LOGICAL_COND_MAP = Map("==" -> "===", "<=" -> "<=", ">=" -> ">=", "<" -> "<=", ">" -> ">", "===" -> "===", "!=" -> "!=")
 
   /**
    * Scala Spark Code
@@ -21,7 +22,7 @@ case class DataFlowFilter(inputVarName: String, lstExpression: ListExpressionCon
    * @return
    */
   override def scalaSparkCode(): String = {
-    s"""val ${output_assigned_variable}=${inputVarName}.filter(${getListFilters()})"""
+    s"""val ${output_assigned_variable}=${inputVarName}.filter(${getScalaListFilters()})"""
   }
 
   /**
@@ -30,7 +31,7 @@ case class DataFlowFilter(inputVarName: String, lstExpression: ListExpressionCon
    * @return
    */
   override def pySparkCode(): String = {
-    s"""${output_assigned_variable}=${inputVarName}.filter(${getListFilters()})"""
+    s"""${output_assigned_variable}=${inputVarName}.filter(${getPySparkListFilters()})"""
   }
 
   /**
@@ -40,14 +41,36 @@ case class DataFlowFilter(inputVarName: String, lstExpression: ListExpressionCon
    * TODO: we need to fix multiple issues here, for example left operand can be value and right can be field
    * or both side left and right can be field from dataframe, etc.
    */
-  private def getListFilters(): String = {
+  private def getScalaListFilters(): String = {
     lstExpression.list.map(filter => {
-      val leftOperand = StringUtil.addBrackets(addInputSourceToColumn(filter.operandL))
-      val rightOperand = StringUtil.addBrackets(filter.operandR)
+      val leftOperand = StringUtil.addBrackets(addSparkInputSourceToColumn(filter.scalaOperandL, SCALA))
+      val rightOperand = StringUtil.addBrackets(filter.scalaOperandR)
       val p = if (!filter.logicalCondition.isEmpty) {
-        s"${leftOperand} ${DATAFLOW_LOGICAL_COND_MAP.getOrElse(filter.operator, "===")} ${rightOperand} ${filter.logicalCondition}"
+        val scalaLogicalCondition = DATAFLOW_LOGICAL_COND_MAP_SCALA.getOrElse(filter.logicalCondition, "||")
+        s"${leftOperand} ${DATAFLOW_LOGICAL_OPERATOR_MAP_SCALA.getOrElse(filter.operator, "===")} ${rightOperand} ${scalaLogicalCondition}"
       } else {
-        s"${leftOperand} ${DATAFLOW_LOGICAL_COND_MAP.getOrElse(filter.operator, "===")} ${rightOperand}"
+        s"${leftOperand} ${DATAFLOW_LOGICAL_OPERATOR_MAP_SCALA.getOrElse(filter.operator, "===")} ${rightOperand}"
+      }
+      p
+    }).mkString(" ")
+  }
+
+  /**
+   * get list of filters from filter expression
+   * Assumption - left operand will be field name and right will be a value
+   * It includes logical conditional operator as well.
+   * TODO: we need to fix multiple issues here, for example left operand can be value and right can be field
+   * or both side left and right can be field from dataframe, etc.
+   */
+  private def getPySparkListFilters(): String = {
+    lstExpression.list.map(filter => {
+      val leftOperand = StringUtil.addBrackets(addSparkInputSourceToColumn(filter.pySparkL, PYTHON))
+      val rightOperand = StringUtil.addBrackets(filter.pySparkR)
+      val p = if (!filter.logicalCondition.isEmpty) {
+        val pySparkLogicalCondition = DATAFLOW_LOGICAL_COND_MAP_PY.getOrElse(filter.logicalCondition, "|")
+        s"(${leftOperand} ${DATAFLOW_LOGICAL_OPERATOR_MAP_PY.getOrElse(filter.operator, "===")} ${rightOperand}) ${pySparkLogicalCondition}"
+      } else {
+        s"(${leftOperand} ${DATAFLOW_LOGICAL_OPERATOR_MAP_PY.getOrElse(filter.operator, "===")} ${rightOperand})"
       }
       p
     }).mkString(" ")
@@ -61,8 +84,13 @@ case class DataFlowFilter(inputVarName: String, lstExpression: ListExpressionCon
    * @param list
    * @return
    */
-  private def addInputSourceToColumn(list: List[String]): List[String] = {
-    val lastElem = list.reverse.head
-    list.init :+ s"""${inputVarName}("${lastElem}")"""
+  private def addSparkInputSourceToColumn(listOfOperand: List[String], language: Language): List[String] = {
+    val lastElem = listOfOperand.reverse.head
+    language match {
+      case SCALA =>
+        listOfOperand.init :+ s"""${inputVarName}("${lastElem}")"""
+      case PYTHON =>
+        listOfOperand.init :+ s"""${inputVarName}.${lastElem}"""
+    }
   }
 }
