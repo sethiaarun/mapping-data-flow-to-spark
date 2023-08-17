58945737e2805897eddbc9f79adce71211c7aae1
arunsethia <arunsethia@microsoft.com>
2023-08-17 11:16:02 -0500

    fixed PySpark error, since sort API are different for Scala SPark and PySpark, we need to use language option to generate different set of code.

--- src/main/scala/com/microsoft/azure/adf/dataflow/model/sort/DataFlowSort.scala
+++ src/main/scala/com/microsoft/azure/adf/dataflow/model/sort/DataFlowSort.scala
@@ -1,5 +1,6 @@
 package com.microsoft.azure.adf.dataflow.model.sort
 
+import com.microsoft.azure.adf.dataflow.`type`.LanguageType.{Language, PYTHON, SCALA}
 import com.microsoft.azure.adf.dataflow.model.util.ListKeyValueProperties
 import com.microsoft.azure.adf.dataflow.model.{Expr, SparkCodeGenerator}
 
@@ -14,25 +15,48 @@ import com.microsoft.azure.adf.dataflow.model.{Expr, SparkCodeGenerator}
 case class DataFlowSort(inputVarName: String, lstSortField: List[SortField],
                         sortProperties: ListKeyValueProperties, output_assigned_variable: String) extends Expr
   with SparkCodeGenerator {
+
+  /**
+   * Scala code for Sort transformation
+   *
+   * @return
+   */
   override def scalaSparkCode(): String = {
-    s"""val ${output_assigned_variable}=${inputVarName}.select("*").orderBy(${getSortColumnOperation()})"""
+    s"""val ${output_assigned_variable}=${inputVarName}.select("*").orderBy(${getSortColumnOperation(SCALA)})"""
   }
 
+  /**
+   * PySpark code for Sort transformation
+   *
+   * @return
+   */
   override def pySparkCode(): String = {
-    s"""${output_assigned_variable}=${inputVarName}.select("*").orderBy(${getSortColumnOperation()})"""
+    s"""${output_assigned_variable}=${inputVarName}.select("*").orderBy(${getSortColumnOperation(PYTHON)})"""
   }
 
 
-  private def getSortColumnOperation(): String = {
+  /**
+   * get sort column operations
+   *
+   * @param lang
+   * @return
+   */
+  private def getSortColumnOperation(lang: Language): String = {
     lstSortField.map(sf => {
       val sortDirection: String = sf.nullFirst match {
         case true =>
           // at the end open and closing brackets are require for PySpark
           // for scala spark it is optional
-          s"""${sf.sortDirection}_nulls_first()"""
-        case _ => s"""${sf.sortDirection}()"""
+          s"""${sf.sortDirection}_nulls_first"""
+        case _ => s"""${sf.sortDirection}"""
       }
-      s"""col("${sf.fieldName}").${sortDirection}"""
+      // Scala Spark and PySpark has different API
+      lang match {
+        case PYTHON =>
+          s"""col("${sf.fieldName}").${sortDirection}()"""
+        case SCALA =>
+          s"""col("${sf.fieldName}").${sortDirection}"""
+      }
     }).mkString(",")
   }
 }
